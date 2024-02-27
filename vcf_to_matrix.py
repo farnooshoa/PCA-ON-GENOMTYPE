60e33528bedde830b106aaea9360fa17f230874e
farnooshoa <97453982+farnooshoa@users.noreply.github.com>
2024-02-26 22:31:25 -0500

    Update vcf_to_matrix.py

--- vcf_to_matrix.py
+++ vcf_to_matrix.py
@@ -2,12 +2,14 @@ import numpy as np
 from pysam import VariantFile
 from sklearn import decomposition
 import pandas as pd
+import matplotlib.pyplot as plt
 
-def read_vcf(vcf_filename, max_records=100):
-   genotypes =[]
-   samples =[]
-   Variant_ids=[]
+genotypes =[]
+samples =[]
+Variant_ids=[]
    
+def read_vcf(vcf_filename):
+  
    with VariantFile (vcf_filename) as vcf_reader:
       counter=0
       for record in vcf_reader:
@@ -17,9 +19,8 @@ def read_vcf(vcf_filename, max_records=100):
            samples =[sample for sample in record.samples]
            genotypes.append(alleles)
            Variant_ids.append(record.id)
-          if counter >= 100:
-            break
-   return np.array(genotypes), samples, variant_ids  
+
+   return np.array(genotypes), samples, Variant_ids  
 
 def read_panel(panel_file):
    with open(panel_file) as panel_file:
@@ -30,25 +31,23 @@ def read_panel(panel_file):
     return labels         
 
 def perform_pca(genotypes_matrix):
-   matrix = np.count_nonzero(genotypes,axis=2)
-   matrix = matrix.T
+    matrix = np.count_nonzero(genotypes_matrix, axis=2).T
 
-   pca = decomposition.PCA(n_components=1)
-   pca.fit(matrix)
+    pca = decomposition.PCA(n_components=2)
+    transformed_matrix = pca.fit_transform(matrix)
+    
 
-   singular_values = pca.singular_values_
-   to_plot = pca.transform(matrix)
-
-   return matrix, singular_values, to_plot
+    return transformed_matrix
 
 
-def create_dataframe(matrix, variant_ids, samples, labels):
+def create_dataframe(matrix, Variant_ids, samples, labels):
    df = pd.DataFrame(matrix,columns=Variant_ids, index=samples)
    df['Population code'] = df.index.map(labels)
    return df
 
 def save_to_csv(df, output_filename="matrix.csv"):
     df.to_csv(output_filename)
+
 
 # Main part of the script
 vcf_filename = "ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf"
@@ -57,7 +56,10 @@ panel_file = "phase1_integrated_calls.20101123.ALL.panel"
 genotypes_matrix, samples, variant_ids = read_vcf(vcf_filename)
 labels = read_panel(panel_file)
 
-matrix, singular_values, to_plot = perform_pca(genotypes_matrix)
+transformed_matrix= perform_pca(genotypes_matrix)
 
-df = create_dataframe(matrix, variant_ids, samples, labels)
+# Create DataFrame and save to CSV
+df = create_dataframe(transformed_matrix, variant_ids, samples, labels)
 save_to_csv(df)
+
+print(df[100])
